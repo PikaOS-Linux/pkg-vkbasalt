f5a40ad6cb5e53e3bed65cea656af90790513ee1
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-08 19:44:26 +0300

    install build-deps on i386

--- main32.sh
+++ main32.sh
@@ -13,6 +13,7 @@ cd ./vkbasalt
 apt-get install build-essential -y
 apt-get install crossbuild-essential-i386 lib32gcc-11-dev -y
 apt-get build-dep ./ -y -a i386
+apt-get -y install meson pkg-config glslang-tools:i386 libx11-dev:i386 libvulkan-dev:i386 spirv-headers
 
 # Build package
 dpkg-buildpackage -a i386
